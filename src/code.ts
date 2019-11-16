9d2df29f056b04b1b1b1c5311436b551f9cbe7bf
unknown <joshi.karthik@yahoo.com>
2019-11-17 00:48:50 +0530

    BehaviorSubject

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,42 @@
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+
+
+var subject = new BehaviorSubject("First")
+
+subject.subscribe(
+    (data)=> addItem('Observer 1 '+data),
+    (err)=> addItem(err),
+    ()=> addItem('observer 1 completed')
+)
+
+subject.next('the first thing has been sent')
+subject.next('about to subscibe 2nd observer')
+
+var observer2 = subject.subscribe(
+    (data)=> addItem('Observer 2'+data),
+    (err)=> addItem(err),
+    ()=> addItem('observer 1 completed')
+)
+
+subject.next('The second thing has been sent')
+subject.next('The third thing has been sent')
+
+observer2.unsubscribe()
+subject.next('The final thing has been sent')
+
+
+function addItem(val:any){
+    var node = document.createElement("li")
+    var textNode = document.createTextNode(val);
+    node.appendChild(textNode);
+    document.getElementById("output").appendChild(node);
+}
+
+
+/*
+Subject
+*/
+/*
 import {Subject} from "rxjs/Subject";
 
 
@@ -22,15 +61,12 @@ subject.next('The third thing has been sent')
 
 observer2.unsubscribe()
 subject.next('The final thing has been sent')
+*/
 
 
-function addItem(val:any){
-    var node = document.createElement("li")
-    var textNode = document.createTextNode(val);
-    node.appendChild(textNode);
-    document.getElementById("output").appendChild(node);
-}
-
+/*
+Observer
+*/
 
 /*
 var observable = fromEvent(document,'mousemove')
